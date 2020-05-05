6b6106da9afa9b5041b670e1f35515075f2e91a0
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-05-05 08:19:33 -0400

    Update mode line tests

--- test/specs/test-mode-line.el
+++ test/specs/test-mode-line.el
@@ -27,19 +27,40 @@
 
 (require 'flycheck-buttercup)
 
-(describe "Mode Line"
+(describe "Mode Line (interactive-only)"
+  (when noninteractive
+    ;; `format-mode-line' doesn't work in batch mode
+    (signal 'buttercup-pending "SKIPPED"))
+
   (it "shows the number of errors and warnings"
     (let ((flycheck-current-errors
            (list (flycheck-error-new-at 1 1 'warning "warning 1")
+                 (flycheck-error-new-at 1 1 'info "info")
                  (flycheck-error-new-at 2 2 'warning "warning 2")
                  (flycheck-error-new-at 1 1 'error "error"))))
       (expect (flycheck-mode-line-status-text 'finished)
-              :to-equal " FlyC:1/2")))
+              :to-equal " FlyC[1 2 1]")))
 
-  (it "does not show the number of infos"
+  (it "omits unnecessary levels"
+    (let ((flycheck-current-errors
+           (list (flycheck-error-new-at 1 1 'error "error"))))
+      (expect (flycheck-mode-line-status-text 'finished)
+              :to-equal " FlyC[1]"))
+    (let ((flycheck-current-errors
+           (list (flycheck-error-new-at 1 1 'warning "warning")
+                 (flycheck-error-new-at 1 1 'error "error"))))
+      (expect (flycheck-mode-line-status-text 'finished)
+              :to-equal " FlyC[1 1]")))
+
+  (it "includes higher-priority levels when lower-priority ones are present"
+    (let ((flycheck-current-errors
+           (list (flycheck-error-new-at 1 1 'warning "warning"))))
+      (expect (flycheck-mode-line-status-text 'finished)
+              :to-equal " FlyC[0 1]"))
     (let ((flycheck-current-errors
            (list (flycheck-error-new-at 1 1 'info "info"))))
-      (expect (flycheck-mode-line-status-text 'finished) :to-equal " FlyC")))
+      (expect (flycheck-mode-line-status-text 'finished)
+              :to-equal " FlyC[0 0 1]")))
 
   (it "includes the prefix"
     (let ((flycheck-mode-line-prefix "foobar")
